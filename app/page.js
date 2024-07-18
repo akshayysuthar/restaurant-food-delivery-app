75f77049cf51514ca8787a7840d8116a241c6907
akshay <akshaysuthar05@gmail.com>
2024-07-18 22:23:56 +0530

    home page done

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="overflow-x-hidden">
       <Slider />
       <Featured />
       <Offer />
